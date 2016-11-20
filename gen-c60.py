9a6e13204ed3f13f3affa1a23e8f05dd01a06e87
vic <vic.trouble@gmail.com>
2016-11-20 13:47:24 +0100

    work in progress

---
+++ gen-c60.py
@@ -0,0 +1,57 @@
+import os
+import sys
+
+try:
+    import bpy
+    from mathutils import Vector
+except ImportError:
+    print('launching blender')
+    os.system(r'"C:\Program Files\Blender Foundation\Blender\blender.exe" --python ' + __file__)
+    sys.exit()
+
+# TODO: set quality
+
+from math import pi, sqrt, sin, cos, tan
+
+def clear_all():
+    bpy.ops.object.select_by_type(type='MESH')
+    bpy.ops.object.delete()
+
+def create_atom(pos):
+    bpy.ops.mesh.primitive_uv_sphere_add(location=pos)
+
+def create_bound(pos1, pos2):
+    dir = Vector(pos2[i] - pos1[i] for i in range(3))
+    #bpy.context.object.rotation_mode = 'QUATERNION'
+    length = dir.length
+    origin = (Vector(pos1) + dir / 2).to_tuple()
+    bpy.ops.mesh.primitive_cylinder_add(radius=0.5, location=origin, depth=length) # rotation=dir.to_track_quat('X', 'Y').to_euler(), 
+    
+    
+def build_c60(bound_length):
+    t = bound_length
+    R5 = t * sqrt(10) * sqrt(5 + sqrt(5)) / 10
+    
+    # build base pentagon
+    base_atoms = []
+    for i in range(5):
+        a = 2 * pi / 5 * i
+        x, y = R5 * sin(a), R5 * cos(a)
+        base_atoms.append((x, y, 0))
+        create_atom(base_atoms[-1])
+        
+    for i in range(5):
+        create_bound(base_atoms[i], base_atoms[(i + 1) % 5])
+    
+    
+def render(filename):
+    bpy.context.scene.render.filepath = filename
+    bpy.context.scene.render.resolution_x = 1024 #perhaps set resolution in code
+    bpy.context.scene.render.resolution_y = 768
+    bpy.ops.render.render(write_still=True)
+        
+clear_all()
+build_c60(10)
+
+render(os.path.join(os.getcwd(), '1.png'))
+sys.exit()
